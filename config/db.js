9ea8850b91e89785f254acd402efca7bc22af364
kshitij-hash <83551355+kshitij-hash@users.noreply.github.com>
2024-07-22 23:24:22 +0530

    backend code

---
+++ config/db.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(
+            `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@wizarmarkcluster.da1si6x.mongodb.net/WizarMark_DB?retryWrites=true&w=majority&appName=WizarMarkCluster`
+        );
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (e) {
+        console.log(`Error: ${e.message}`);
+        process.exit(1);
+    }
+}
+
+export default connectDB;
+
+
