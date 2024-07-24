18b47761db27cd2e04afba187d2ff156d10a2043
Harshita <harshitalkadeep@gmail.com>
2024-07-24 11:10:04 +0530

    test3 commit

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,71 @@
+// import express from 'express';
+// import dotenv from 'dotenv';
+// import path from 'path';
+// import cors from 'cors';
+
+// // import {userRoute} from './routes/user.js';
+// import { aiRoute } from './routes/ai.js'; 
+
+
+// dotenv.config({ path: new URL('../.env', import.meta.url).pathname });
+
+// import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+
+// const app = express();
+// app.use(cors());
+
+// // import connectDB from './config/db.js';
+
+// app.use(express.json());
+
+// // app.use("/api/user", userRoute);
+// app.use("/api/ai", aiRoute);
+
+// app.get('/', (req, res) => {
+//   res.send('API is running...');
+// });
+
+
+// app.use(notFound);
+// app.use(errorHandler);
+
+// const port = process.env.PORT || 7070;
+
+// app.listen(port, () => {
+//   console.log(`Server is running on port ${port}`);
+// });
+
+// // connectDB()
+// //   .then(async () => {
+// //     app.listen(port, () => {
+// //       console.log(`Server is running on port ${port}`);
+// //     });
+// //   })
+// //   .catch((error) => {
+// //     console.log("Error connecting to the database", error);
+// //   });
+
+
+
 import express from 'express';
 import dotenv from 'dotenv';
 import path from 'path';
 import cors from 'cors';
 
-// import {userRoute} from './routes/user.js';
-import { aiRoute } from './routes/ai.js'; 
-
-
-dotenv.config({ path: new URL('../.env', import.meta.url).pathname });
+dotenv.config();
 
 import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import { aiRoute } from './routes/ai.js'; 
 
 const app = express();
 app.use(cors());
-
-// import connectDB from './config/db.js';
-
 app.use(express.json());
 
-// app.use("/api/user", userRoute);
 app.use("/api/ai", aiRoute);
 
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
-
 
 app.use(notFound);
 app.use(errorHandler);
@@ -35,12 +76,3 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-// connectDB()
-//   .then(async () => {
-//     app.listen(port, () => {
-//       console.log(`Server is running on port ${port}`);
-//     });
-//   })
-//   .catch((error) => {
-//     console.log("Error connecting to the database", error);
-//   });
